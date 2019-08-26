06ca2707cd737bbfe108bd3e8175f14078765fc6
Stijn <s.heldens@esciencecenter.nl>
2019-08-26 17:21:38 +0200

    Extended documentation for Document class.

--- litstudy/common.py
+++ litstudy/common.py
@@ -2,29 +2,45 @@
 
 class Document:
   """Example of document"""
+
   def __init__(self, **kwargs):
     """ Initialize document """
 
-    self.doi = kwargs.get('doi')
-    self.title = kwargs.get('title')
-    self.authors = kwargs.get('authors')
-    self.keywords = kwargs.get('keywords')
-    self.abstract = kwargs.get('abstract')
-    self.references = kwargs.get('references')
-    self.year = kwargs.get('year')
-    self.source = kwargs.get('source')
+    self.doi = kwargs.pop('doi', None)
+    """ Digital Object Identifier (DOI) of the document, or `None` if unavailable. """
 
-    self.citation_count = kwargs.get('citation_count')
-    """ The number of citations """
+    self.title = kwargs.pop('title', None)
+    """ Title of document. """
 
-    self._internal = kwargs.get('internal')
+    self.authors = kwargs.pop('authors', None)
+    """ Authors of document as list of `Author` objects, or `None` if unavailable. """
+
+    self.keywords = kwargs.pop('keywords', None)
+    """ List of author specified keywords, or `None` if unavailable. """
+
+    self.abstract = kwargs.pop('abstract', None)
+    """ Abstract of document, or `None` if unavailable. """
+
+    self.references = kwargs.pop('references', None)
     """ """
+
+    self.year = kwargs.pop('year', None)
+    """ Year of publication as integer. """
+
+    self.source = kwargs.pop('source', None)
+    """ Name of source (for example: 'The International Conference on Knowledge Discovery and Data Mining') or `None` if unavailable. """
+
+    self.citation_count = kwargs.pop('citation_count', None)
+    """ The number of received citations, or None if unavailable. """
+
+    self._internal = kwargs.pop('internal', None)
+    """ Internal object used to extract these properties  """
 
 class Author:
   def __init__(self, **kwargs):
-    self.orcid = kwargs.get('orcid')
-    self.name = kwargs.get('name')
-    self.affiliations = kwargs.get('affiliations')
+    self.orcid = kwargs.pop('orcid')
+    self.name = kwargs.pop('name')
+    self.affiliations = kwargs.pop('affiliations')
 
 class Affiliation:
   #name
