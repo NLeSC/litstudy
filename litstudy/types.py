1eaba2a69858d9761307f38f7896da36dab912c4
Stijn <s.heldens@esciencecenter.nl>
2022-01-10 21:04:06 +0100

    Add documentation in types module

--- litstudy/types.py
+++ litstudy/types.py
@@ -10,7 +10,30 @@ from .common import fuzzy_match, canonical, progress_bar
 
 
 class DocumentSet:
+    """ Represents a set of documents.
+
+    `DocumentSet` stores a list of `Document` objects. Optionally, a pandas 
+    data frame can be provided which stores additional annotations on the 
+    documents.
+
+    All set operations are accepted by `DocumentSet` (union, intersection,
+    difference), allowing for new sets to be created from existing sets.
+
+    Note that a `DocumentSet` is immutable and its content cannot be changed.
+    Instead, most methods below return a new `DocumentSet` instead of 
+    performing modifications in-place.
+    """
+
     def __init__(self, docs, data=None):
+        """ Construct a new `DocumentSet`.
+
+        :param docs: A list (or iterator) of `Document` objects.
+        :param data: Additional metadata associated with the documents. This
+                     can be either a `pandas.DataFrame` or something which
+                     is accepted by the pandas DataFrame constructor.
+        """
+        docs = list(docs)
+
         if data is None:
             data = pd.DataFrame(index=range(len(docs)))
         else:
@@ -40,20 +63,43 @@ class DocumentSet:
         return DocumentSet(new_docs, new_data), DocumentSet(old_docs, old_data)
 
     def add_column(self, name: str, values) -> DocumentSet:
+        """ Returns a new set which has an additional column added.
+
+        :param name: Name of the new column.
+        :param values: List of values. Should be the same length as the
+                       number of documents in this set.
+        :returns: The new document set.
+        """
         assert len(values) == len(self.docs)
         data = self.data.copy(deep=False)
         data[name] = values
         return DocumentSet(self.docs, data)
 
     def remove_column(self, name: str) -> DocumentSet:
+        """ Returns a new set which has the given column removed.
+
+        :param name: Name of the column.
+        :returns: The new document set.
+        """
         data = self.data.copy(deep=False)
         data.drop(name)
         return DocumentSet(self.docs, data)
 
     def filter_docs(self, predicate) -> DocumentSet:
+        """ Returns a new set for which the provided predicate returned `True`.
+
+        :param predicate: A function `Document -> bool`.
+        :returns: The new document set.
+        """
         return self.filter_meta(lambda doc, _: predicate(doc))
 
     def filter(self, predicate) -> DocumentSet:
+        """ Returns a new set for which the provided predicate returned `True`.
+
+        :param predicate: A function `Document, dict -> bool`. The provided
+                          dict stores the annotations of the document.
+        :returns: The new document set.
+        """
         indices = []
 
         for doc, record in zip(self.docs, self.data.itertuples()):
@@ -63,12 +109,25 @@ class DocumentSet:
         return self.select(indices)
 
     def select(self, indices) -> DocumentSet:
+        """ Returns a new set which contains only the documents at the
+        provided indices.
+
+        :param indices: Any input accepted by `pandas.DataFrame.iloc` such
+                        as a list of integer.
+        :returns: The new document set.
+        """
+
         data = self.data.iloc[indices]
         docs = [self.docs[i] for i in data.index]
         data = data.reset_index(drop=True)
         return DocumentSet(docs, data)
 
     def difference(self, other: DocumentSet) -> DocumentSet:
+        """ Returns a new set which does contain the documents provided in
+        `self` but not in `other`. This is also available as the `-` operator.
+
+        :returns: The new document set.
+        """
         haystack = [d.id for d in other]
         indices = []
 
@@ -80,6 +139,11 @@ class DocumentSet:
         return self.select(indices)
 
     def intersect(self, other: DocumentSet) -> DocumentSet:
+        """ Returns a new set which does contain the documents provided in
+        both `self` and `other`. This is also available as the `&` operator.
+
+        :returns: The new document set.
+        """
         haystack = [d.id for d in other]
         indices = []
 
@@ -91,6 +155,12 @@ class DocumentSet:
         return self.select(indices)
 
     def concat(self, other: DocumentSet) -> DocumentSet:
+        """ Returns a new set which does contain the documents provided in
+        either `self` and `other`. Duplicate documents are not removed, see
+        `union` instead. This is also available as the `+` operator.
+
+        :returns: The new document set.
+        """
         # TODO: We need a better way to deal to handle the case where
         # the columns are not equal in some way.
         data = pd.concat([self.data, other.data])
@@ -99,6 +169,13 @@ class DocumentSet:
         return DocumentSet(docs, data)
 
     def union(self, other: DocumentSet) -> DocumentSet:
+        """ Returns a new set which does contain the documents provided in
+        either `self` and `other`. Duplicate documents in `other` that also
+        appear in `self` are discarded. This is also available as the `|`
+        operator.
+
+        :returns: The new document set.
+        """
         n = len(self)
         indices = list(range(n))
         haystack = [d.id for d in self]
@@ -112,6 +189,10 @@ class DocumentSet:
         return self.concat(other).select(indices)
 
     def unique(self) -> DocumentSet:
+        """ Returns a new set which has all duplicate documents removed.
+
+        :returns: The new document set.
+        """
         indices = []
 
         for i, doc in enumerate(self.docs):
@@ -130,32 +211,52 @@ class DocumentSet:
         return self.select(indices)
 
     def sample(self, n, seed=0) -> DocumentSet:
+        """ Returns a new set which contains `n` randomly chosen documents
+        from `self`.
+
+        :returns: The new document set.
+        """
         if len(self) <= n:
             return self
 
+        random.seed(seed)
         indices = random.sample(len(self), n)
         indices.sort()
         return self.select(indices)
 
     def itertuples(self):
+        """ Returns an iterator over `(Document, dict)` tuples, where the
+        `dict` contains the annotations of this document.
+        """
         return zip(self.docs, self.data.itertuples())
 
     def __or__(self, other):
+        """ Alias for `DocumentSet.union` """
         return self.union(other)
 
     def __and__(self, other):
+        """ Alias for `DocumentSet.intersect` """
         return self.intersect(other)
 
     def __add__(self, other):
+        """ Alias for `DocumentSet.concat` """
         return self.concat(other)
 
     def __sub__(self, other):
+        """ Alias for `DocumentSet.difference` """
         return self.difference(other)
 
     def __len__(self):
+        """ Returns the number of documents in this set """
         return len(self.docs)
 
     def __getitem__(self, key):
+        """ Returns different things depending on the key type:
+
+        * `str`: The column named `key` is returned.
+        * `int`: The document at position `key` is returned.
+        * otherwise: The call is forwarded to `DocumentSet.select`.
+        """
         if isinstance(key, str):
             return self.data[key]
         elif isinstance(key, int):
@@ -164,13 +265,26 @@ class DocumentSet:
             return self.select(key)
 
     def __iter__(self):
+        """ Returns an iterator over `Document` objects in this set. """
         return iter(self.docs)
+
+    def __bool__(self):
+        return bool(len(self))
 
     def __repr__(self):
         return repr(self.docs)
 
 
 class DocumentIdentifier:
+    """ Represents an identifier for a document.
+
+    Uniquely identifing an scientific document is often difficult since a
+    single document might have multiple identifiers assigned to it (e.g., DOI,
+    PubMed ID, Scopus ID, SemanticScholar ID) and not all data sources might
+    provide all these identifiers. This class stores all possible identifiers
+    that a document has.
+    """
+
     def __init__(self, title, **attr):
         # Remove keys where value is None
         self._title = title
@@ -178,33 +292,48 @@ class DocumentIdentifier:
 
     @property
     def title(self) -> Optional[str]:
+        """ Returns the title. """
         return self._title
 
     @property
     def doi(self) -> Optional[str]:
+        """ Returns the DOI (example: 10.1093/ajae/aaq063). """
         return self._attr.get('doi')
 
     @property
-    def isbn(self):
+    def isbn(self) -> Optional[str]:
+        """ Returns the ISBN. """
         return self._attr.get('isbn')
 
     @property
-    def pubmed(self):
+    def pubmed(self) -> Optional[str]:
+        """ Returns the PubMed ID. """
         return self._attr.get('pubmed')
 
     @property
-    def arxivid(self):
+    def arxivid(self) -> Optional[str]:
+        """ Returns the arXiv ID. """
         return self._attr.get('arxivid')
 
     @property
-    def scopusid(self):
+    def scopusid(self) -> Optional[str]:
+        """ Returns the Scopus ID. """
         return self._attr.get('eid')
 
     @property
-    def s2id(self):
+    def s2id(self) -> Optional[str]:
+        """ Returns the Semantic Scholar ID. """
         return self._attr.get('s2id')
 
-    def matches(self, other):
+    def matches(self, other: DocumentIdentifier) -> bool:
+        """ Returns `True` iff these two identifiers are equivalent
+
+
+        Two documents are considered to be equivalent if all identifiers they
+        have in common are equal. For example, if both documents have a DOI
+        then these should be the same. If two documents have not a single
+        identifier in common, a fuzzy match based on the title is performed.
+        """
         n = 0
 
         # Two identifiers match if all keys that they have in common are equal
@@ -217,9 +346,13 @@ class DocumentIdentifier:
         if n > 0:
             return True
 
+        # No identifiers in common
         return fuzzy_match(self._title, other._title)
 
     def merge(self, other) -> 'DocumentIdentifier':
+        """ Returns a new `DocumentIdentifier` which adds the identifiers
+        `others` to `self`.
+        """
         attr = dict()
         attr.update(other._attr)
         attr.update(self._attr)
@@ -230,25 +363,35 @@ class DocumentIdentifier:
 
 
 class Document(ABC):
+    """ Stores the metadata of a document.
+
+    This is an interface which provides several methods which can be
+    overridden by child classes. All methods can thus return `None`
+    in case that method is not overridden.
+    """
     def __init__(self, identifier: DocumentIdentifier):
         self._identifier = identifier
 
     @property
     def id(self) -> DocumentIdentifier:
+        """ The `DocumentIdentifier` of this document. """
         return self._identifier
 
     @property
     @abstractmethod
     def title(self) -> str:
+        """ The title of this document. """
         pass
 
     @property
     @abstractmethod
-    def authors(self):
+    def authors(self) -> Optional[List[Author]]:
+        """ The authors of this document. """
         pass
 
     @property
-    def affiliations(self):
+    def affiliations(self) -> Optional[List[Affiliation]]:
+        """ The affiliations associated with the authors of this document. """
         authors = self.authors
 
         if authors is not None:
@@ -263,49 +406,67 @@ class Document(ABC):
         return None
 
     @property
-    def publisher(self):
+    def publisher(self) -> Optional[str]:
+        """ The publisher of this document. """
         return None
 
     @property
-    def language(self):
+    def language(self) -> Optional[str]:
+        """ The language this document is written in. """
         return None
 
     @property
     def publication_date(self) -> Optional[date]:
+        """ The data of publication. """
         return None
 
     @property
-    def publication_year(self):
+    def publication_year(self) -> Optional[int]:
+        """ The year of publication. """
         date = self.publication_date
         if date is None:
             return None
         return date.year
 
     @property
-    def publication_source(self):
+    def publication_source(self) -> Optional[str]:
+        """ The name of the publication source (i.e., journal name,
+        conference name, etc.)
+        """
         return None
 
     @property
-    def keywords(self):
+    def keywords(self) -> Optional[List[str]]:
+        """ The keywords of this document. What exactly consistutes as
+        keywords depends on the data source (author keywords, generated
+        keywords, topic categories), but is should be a list of strings.
+        """
         return None
 
     @property
-    def abstract(self):
+    def abstract(self) -> Optional[str]:
+        """ The abstract of this document. """
         return None
 
     @property
     def citation_count(self) -> Optional[int]:
+        """ The number of citations that this document received. """
         return None
 
     @property
     def references(self) -> Optional[List[DocumentIdentifier]]:
+        """ The list of other documents that are cited by this document. """
         return None
 
     @property
     def citations(self) -> Optional[List[DocumentIdentifier]]:
+        """ The list of other documents that cite this document. """
         return None
 
-    def mentions(self, term):
+    def mentions(self, term: str) -> bool:
+        """ Returns `True` if this document mentions the given term in the
+        title, abstract, or keywords.
+        """
         pattern = r'(^|\s)' + re.escape(term) + r'($|\s)'
         flags = re.IGNORECASE
         keywords = self.keywords or []
@@ -318,36 +479,45 @@ class Document(ABC):
 
 
 class Affiliation(ABC):
+    """ Represents the affiliation of an author """
     @property
     @abstractmethod
     def name(self) -> str:
+        """ Name of the affiliation """
         pass
 
     @property
     def city(self) -> Optional[str]:
+        """ City the affiliation is located in. """
         pass
 
     @property
     def country(self) -> Optional[str]:
+        """ Country the affiliation is located in. """
         pass
 
 
 class Author(ABC):
+    """ Represents the author of a document. """
     @property
     @abstractmethod
-    def name(self):
+    def name(self) -> str:
+        """ The name of the author. """
         pass
 
     @property
-    def orcid(self):
+    def orcid(self) -> Optional[str]:
+        """ The ORCID of the author. """
         return None
 
     @property
-    def s2id(self):
+    def s2id(self) -> Optional[str]:
+        """ The SemanticScholar ID of the author. """
         return None
 
     @property
     def affiliations(self) -> 'Optional[list[Affiliation]]':
+        """ The affiliations this author is associated with. """
         return None
 
 
