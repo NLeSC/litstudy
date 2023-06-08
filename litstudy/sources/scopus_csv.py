d875c3de33667d6e5418e6921c0907b715f716ab
Stijn <s.heldens@esciencecenter.nl>
2023-06-08 08:53:04 +0200

    Simplified some of the if-statements in `scopus_csv.py`

--- litstudy/sources/scopus_csv.py
+++ litstudy/sources/scopus_csv.py
@@ -42,17 +42,14 @@ class ScopusCsvDocument(Document):
 
     @property
     def title(self) -> Optional[str]:
-        title = self.entry.get("Title")
-        if title == "":
-            return None
-        return title
+        return self.entry.get("Title") or None
 
     @property
     def authors(self) -> List[ScopusCsvAuthor]:
         auths_affs = self.entry.get("Authors with affiliations")
         auths_id = self.entry.get("Author(s) ID", "")
         # author_last, first initial, affiliation; .....
-        if auths_affs == "" or auths_affs is None:
+        if not auths_affs:
             return []
         auths_affs = auths_affs.split("; ")
         auths = [", ".join(auth_aff.split(", ")[0:2]) for auth_aff in auths_affs]
@@ -65,59 +62,51 @@ class ScopusCsvDocument(Document):
 
     @property
     def publisher(self) -> Optional[str]:
-        pub = self.entry.get("Publisher")
-        if pub == "":
-            return None
-        return pub
+        return self.entry.get("Publisher") or None
 
     @property
     def publication_year(self) -> Optional[int]:
         year = self.entry.get("Year")
-        if year == "" or year is None:
+        if not year:
             return None
-        return int(year)
+
+        try:
+            return int(year)
+        except:
+            return None
 
     @property
     def keywords(self) -> Optional[List[str]]:
         keywords = self.entry.get("Author Keywords")
-        if keywords == "":
+        if not keywords:
             return None
         return keywords.split("; ")
 
     @property
     def abstract(self) -> Optional[str]:
         abstract = self.entry.get("Abstract")
-        if abstract == "":
+        if not abstract:
             return None
         return abstract
 
     @property
     def citation_count(self) -> Optional[int]:
         citation_count = self.entry.get("Cited by")
-        if citation_count == "" or citation_count is None:
+        if not citation_count:
             return None
         return int(citation_count)
 
     @property
     def language(self) -> Optional[str]:
-        language = self.entry.get("Language of Original Document")
-        if language == "":
-            return None
-        return language
+        return self.entry.get("Language of Original Document") or None
 
     @property
     def publication_source(self) -> Optional[str]:
-        pub_source = self.entry.get("Source title")
-        if pub_source == "":
-            return None
-        return pub_source
+        return self.entry.get("Source title") or None
 
     @property
     def source_type(self) -> Optional[str]:
-        doc_type = self.entry.get("Document Type")
-        if doc_type == "":
-            return None
-        return doc_type
+        return self.entry.get("Document Type") or None
 
 
 def load_scopus_csv(path: str) -> DocumentSet:
