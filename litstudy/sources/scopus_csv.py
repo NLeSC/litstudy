74cccdbe2ded44862dc8166c926f0844e68ca221
Travis <tleedepriest@gmail.com>
2023-08-11 16:11:55 -0400

    added support for multiple affiliations and new scopus.csv format.

--- litstudy/sources/scopus_csv.py
+++ litstudy/sources/scopus_csv.py
@@ -17,9 +17,9 @@ class ScopusCsvAffiliation(Affiliation):
 
 
 class ScopusCsvAuthor(Author):
-    def __init__(self, name, affiliation):
+    def __init__(self, name, affiliations):
         self._name = name
-        self._affiliation = affiliation
+        self._affiliations = affiliations
 
     @property
     def name(self):
@@ -27,7 +27,7 @@ class ScopusCsvAuthor(Author):
 
     @property
     def affiliations(self):
-        return [ScopusCsvAffiliation(self._affiliation)]
+        return self._affiliations
 
 
 class ScopusCsvDocument(Document):
@@ -44,21 +44,105 @@ class ScopusCsvDocument(Document):
     def title(self) -> Optional[str]:
         return self.entry.get("Title") or None
 
+    def _get_authors(self) -> Optional[List[str]]:
+        """
+        helper method. parse two formats of Authors field.
+        Formats:
+            1. deliminates author by , and [No author name available]
+            2. deliminates author by ; and [No Authors found]
+        """
+        auths = self.entry.get("Authors")
+        no_authors_formats = ["[No Authors Found]", "[No author name available]"]
+
+        if auths in no_authors_formats:
+            return []
+
+        if ";" in auths:
+            author_list = auths.split("; ")
+        elif "," in auths:
+            author_list = auths.split(", ")
+            # add comma between auth last, first to match format
+            # in authors with affiliations
+            author_list = [', '.join(auth.rsplit(" ", 1)) for auth in author_list]
+        else: # single author...
+            author_list = [auths]
+
+        return author_list
+
+    def _get_authors_ids(self) -> Optional[List[str]]:
+        """
+        helper method to parse two formats of
+            'Author(s) ID' field
+        1. AUTHOR_ID;AUTHOR_ID_2;LAST_AUTHOR_ID;
+        2. AUTHOR_ID; AUTHOR_ID_2; LAST AUTHOR_ID
+        """
+        auths_id = self.entry.get("Author(s) ID", "")
+        if auths_id[-1] == ";":
+            auths_id = auths_id[:-1]
+        auths_ids = auths_id.split(";")
+        auths_ids = [auth_id.lstrip().rstrip() for auth_id in auths_ids]
+        return auths_ids
+
+    def _get_auths_with_ids(self):
+        auths = self._get_authors()
+        auths_id = self._get_authors_ids()
+
+        if len(auths) == len(auths_id):
+            auths_w_ids = [f"{name} (ID: {auth_id})" for name, auth_id in zip(auths, auths_id)]
+        else:
+            auths_w_ids = []
+        return auths_w_ids
+
+    @staticmethod
+    def _get_affiliations(affiliation_substring):
+        affiliations = affiliation_substring.split(";")
+        affiliations = [
+            ScopusCsvAffiliation(aff) for aff in affiliations]
+        return affiliations
+
     @property
     def authors(self) -> List[ScopusCsvAuthor]:
+        auths = self._get_authors()
         auths_affs = self.entry.get("Authors with affiliations")
-        auths_id = self.entry.get("Author(s) ID", "")
+        auths_ids = self._get_authors_ids()
+        affs = self.entry.get("Affiliations")
         # author_last, first initial, affiliation; .....
-        if not auths_affs:
-            return []
-        auths_affs = auths_affs.split("; ")
-        auths = [", ".join(auth_aff.split(", ")[0:2]) for auth_aff in auths_affs]
-        affs = [", ".join(auth_aff.split(", ")[2:]) for auth_aff in auths_affs]
         # try to add id to author name
-        auths_id = auths_id.split(";")[:-1]  # remove empty string last el
-        if len(auths) == len(auths_id):
-            auths = [f"{name} (ID: {auth_id})" for name, auth_id in zip(auths, auths_id)]
-        return [ScopusCsvAuthor(a, b) for a, b in zip(auths, affs)]
+        if not auths_affs or auths is None or auths_ids is None:
+            return []
+
+        auths_w_ids = self._get_auths_with_ids()
+        # if single author, no way to know if ',' in author name
+        # within auths_affs field,
+        # so need to use 'Affiliations' field instead
+        # of searching for author in auths_affs
+        if len(auths) == 1:
+            return [ScopusCsvAuthor(auths[0], self._get_affiliations(affs))]
+        print(auths)
+        print(auths_affs)
+        indexes_of_authors = [auths_affs.index(auth) for auth in auths]
+        auth_to_affs_mapping = {}
+        for num, index in enumerate(indexes_of_authors):
+            auth = auths[num]
+            if num < len(indexes_of_authors) - 1:
+
+                next_index = indexes_of_authors[num+1]
+                substring = auths_affs[index:next_index]
+                # only want part of string for current auhtor
+                # and affiliations
+            else:
+                substring = auths_affs[index:]
+
+            substring = substring.replace(f"{auth}, ", "")
+            affiliations = substring.split(";")
+            affiliations = [
+                ScopusCsvAffiliation(aff) for aff in affiliations]
+            if auths_w_ids!=[]:
+                auth_to_affs_mapping[auths_w_ids[num]] = affiliations
+            else:
+                auth_to_affs_mapping[auth] = affiliations
+
+        return [ScopusCsvAuthor(a, b) for a, b in auth_to_affs_mapping.items()]
 
     @property
     def publisher(self) -> Optional[str]:
